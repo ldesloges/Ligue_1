a1e658c801be610c457320cff4feac92731603ce
DESLOGES <ldesloges4@gmail.com>
2026-01-17 18:18:12 +0100

    Mise à jour simulation Ligue 1

--- app.py
+++ app.py
@@ -637,7 +637,7 @@ fig.update_layout(
     updatemenus=[dict(
         type="buttons",
         showactive=False,
-        x=0, y=1.2,
+        x=0, y=-0.1,
         buttons=[dict(
             label="▶ Lancer la simulation",
             method="animate",
